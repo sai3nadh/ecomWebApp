9679baf0c9a904418d2f562d22c41a241866ce3a
Trinadh Rongala <sai3nadh@gmail.com>
2024-08-07 19:29:01 +0100

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
+  appVersion: packageInfo.version,
     production: true,
     apiUrl: 'https://api.example.com'  // Production API URL
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
+    appVersion: packageInfo.version,
     production: false,
     apiUrl: 'https://api-dev.example.com'  // Development API URL
   };
