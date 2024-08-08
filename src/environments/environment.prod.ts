a6945bbc3a9ee113431f7aca1f9c124d00631f41
Trinadh Rongala <sai3nadh@gmail.com>
2024-08-08 01:18:23 +0100

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
     production: true,
-    apiUrl: 'https://api.example.com'  // Production API URL
+    apiUrl: 'https://j41myv2bm7.execute-api.eu-west-2.amazonaws.com/v1/'  // Production API URL
   };
   
