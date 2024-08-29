3e1adbd15b9166e966c4919c6c19778945530f5a
Trinadh Rongala <sai3nadh@gmail.com>
2024-08-29 12:36:07 +0100

    validate loginId

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { NavbarService } from './navbar.service'; // Adjust the path as needed
+import { NavbarService } from './navbar.service'; 
 import { StorageService } from '../services/storage.service';
 
 
@@ -18,7 +18,7 @@ import { StorageService } from '../services/storage.service';
 export class NavbarComponent {
 
   categories: any[] = [];
-  profileLinks: any[] = []; // Assuming you have this from another endpoint or static
+  profileLinks: any[] = []; 
   userId: any;
   searchQuery: string = '';
 
@@ -28,6 +28,12 @@ export class NavbarComponent {
   ) { }
 
   ngOnInit(): void {
+    const storedUserId = this.storageService.getLocalVariable('userId');
+    if (!storedUserId) {
+      alert('Please login to continue shopping..');
+      console.log('Please login to continue shopping..');
+      this.router.navigate(['/login']);
+    }
     this.navService.getCategories().subscribe(
       (data: any[]) => {
         this.categories = data;
@@ -36,46 +42,28 @@ export class NavbarComponent {
         console.error('Error fetching categories:', error);
       }
     );
-    // this.navService.getCategories().subscribe(data => {
-    //   this.categories = data;
-    // });
-    // console.log('calling here')
-    // this.printSessionValues();
-    // this.userId = this.storageService.getSessionItem('userId');
-    // console.log("uer id "+this.userId)
-    // if(!this.userId ){
-    //   console.log("inside ");
-    //   alert('You don\'t have valid Active session. Please login again!!' )
-    //   this.router.navigate(['/login']);
-    // }
   }
   stayOnPage(event: Event) {
     event.preventDefault();
-    // Perform your action here
   }
   onSearch(query: string) {
-    // if (query) {
-    //   console.log("enter to query")
-    //   this.router.navigate(['/search'], { queryParams: { q: query } });
-    // }
     if (this.searchQuery.trim()) {
       console.log('calledddd');
       this.router.navigate(['/search'], { queryParams: { query: this.searchQuery } });
     }
   }
   getCategoryLink(id: number): string {
-    return `/categories/${id}`; // Adjust this based on your routing setup
+    return `/categories/${id}`; 
   }
   onCategoryClick(categoryName: string): void {
     // Navigate to the search page with the category name as the query parameter
     this.router.navigate(['/search'], { queryParams: { query: categoryName } });
   }
   cartLink() {
-    // return `/cart`; // Adjust this based on your routing setup
+    
     this.router.navigate(['/cart']);
   }
   signOut() {
-    // this.printSessionValues();
     // Clear all session storage variables
     sessionStorage.clear();
     // Navigate to the login page
@@ -84,12 +72,4 @@ export class NavbarComponent {
     this.storageService.clearSession();
     this.router.navigate(['/login']);
   }
-  // printSessionValues() {
-  //   // this.userId = this.storageService.getSessionItem('userId');
-  //   console.log('UserId:', this.storageService.getSessionItem('userId'));
-  //   console.log('Email:', this.storageService.getSessionItem('email'));
-  //   console.log('Username:', this.storageService.getSessionItem('username'));
-  //   console.log('FirstName:', this.storageService.getSessionItem('firstName'));
-  //   console.log('LastName:', this.storageService.getSessionItem('lastName'));
-  // }
 }
